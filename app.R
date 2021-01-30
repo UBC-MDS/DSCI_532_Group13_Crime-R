4027ad48a45253027149c584c4b0f913942a903f
Charles Suresh <emailcharlesjosh@gmail.com>
2021-01-30 20:06:26 +0530

    initial work

--- app.R
+++ app.R
@@ -4,6 +4,29 @@ library(dashHtmlComponents)
 library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
+library(tidyverse)
+library(stringr)
+
+data_raw <- read.csv("data/raw/ucr_crime_1975_2015.csv")
+
+crime_list <- list('Homicide', 'Rape', 'Larceny', 'Violent')
+sum_list <- list('homs_sum', 'rape_sum', 'rob_sum', 'violent_crime')
+rate_list <- list('homs_per_100k', 'rape_per_100k', 'rob_per_100k', 'violent_per_100k')
+
+data_processing <- function(data){
+  data <- data %>%
+    mutate(Abbreviation = substr(ORI, 1, 2))
+  states <- read.csv('data/raw/states.csv')
+  data %>%
+    left_join(states, by = "Abbreviation") %>%
+    select(-c(source, url, ORI, months_reported))
+}
+
+data_crime <- data_processing(data_raw)
+state_list <- unique(data_crime %>%
+                       select(State) %>%
+                       drop_na() %>%
+                       pull(State))
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
@@ -20,7 +43,7 @@ custom_css <- function() {
   # Footnote text
   css$sources <- list("font-size" = "xx-small")
   
-  css$filter_panel <- list("border" = "10px solid black", )
+  css$filter_panel <- list("border" = "10px solid black")
   
   # Return CSS
   css
@@ -38,7 +61,7 @@ app$layout(
           dbcCol(
             id = 'filter-panel',
             style = css$filter_panel,
-            md = 3,
+            md = 4,
             list(
               htmlBr(),
               dbcLabel("STATE"),
@@ -91,13 +114,35 @@ app$layout(
                   list(label = "Number", value = "number"))
                 )
             )
+          ),
+          dbcCol(
+            id = 'trend_chart',
+            md = 9,
+            list(
+              dccGraph(id='trend_chart_1'),
+              dccGraph(id='trend_chart_2')
+            )
+            
           )
+          
         )
       )
     )
   )
 )
 
-
+app$callback(
+  output('trend_chart_1', 'figure'),
+  list(input('state', 'value'),
+       input('crime', 'value'),
+       input('year_range', 'value'),
+       input('metric', 'value')),
+  function(state, crime, year_range, metric){
+    trend <- data_crime %>%
+      ggplot(aes(x = year, y = homs_sum)) +
+      geom_line(stat = 'summary', fun = sum)
+    
+    ggplotly(trend)
+  })
 
 app$run_server()
